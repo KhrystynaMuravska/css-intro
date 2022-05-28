ddb93fede5253f1309fa600069cc9c716c615f94
Khrystyna Muravska <konukxrustuna@gmail.com>
2022-05-29 00:57:47 +0300

    ++

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,17 @@
-//BOM - Browser Object Model
-//Wondow
-//DOM - document Object Model
-//document
-//document.querySelector("selector css") //повертає 1 елемент
-//document.querySelectorAll('selector css') //повертає масив елементів (array of elements)
-const clock = document.querySelector('.clock');
-clock.innerHTML =  new Date().toLocaleTimeString();
+// BOM - Browser Object Model
+// Wondow
+// DOM - document Object Model
+// document
+// document.querySelector("selector css") //повертає 1 елемент
+// document.querySelectorAll('selector css') //повертає масив елементів (array of elements)
+
+// IIFE - https://developer.mozilla.org/ru/docs/Glossary/IIFE
+
+(function() {
+  const clock = document.querySelector('.clock');
+
+  function updateClock() {
+    clock.innerHTML = new Date().toLocaleTimeString();
+  }
+  setInterval(updateClock, 1000) //час буде оновлюватись раз на 1000 мілісекунд (або 1 секунда)
+}) ();
